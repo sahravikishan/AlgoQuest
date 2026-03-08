68ff23d273d27c792ce421466ec4064b646edca0
Ravikishan <ravik364144@gmail.com>
2026-03-08 19:26:14 +0530

    Shifted database from SQLite3 to Postgress SQL (commit 14)

--- AlgoQuest/settings.py
+++ AlgoQuest/settings.py
@@ -119,13 +119,26 @@ WSGI_APPLICATION = 'AlgoQuest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+USE_POSTGRES = _env_bool('DJANGO_USE_POSTGRES', False)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if USE_POSTGRES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', '').strip(),
+            'USER': os.getenv('POSTGRES_USER', '').strip(),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '').strip(),
+            'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1').strip(),
+            'PORT': os.getenv('POSTGRES_PORT', '5432').strip(),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
