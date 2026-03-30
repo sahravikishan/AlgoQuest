fcaa59aea9f9ecd7fa487787a61a05b26931b4f6
Ravikishan <ravik364144@gmail.com>
2026-03-30 22:45:19 +0530

    Fix Render login messaging and load challenge bank on deploy (commit 23)

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+if [ "${LOAD_CHALLENGE_BANK_ON_BUILD:-true}" = "true" ]; then
+  python manage.py load_challenge_bank
+fi
